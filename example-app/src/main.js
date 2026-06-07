dee5330ac69ddcc4b5b38993cbd5ecb769b68917
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 12:28:34 +0200

    ci: deploy example app to capgo

--- example-app/src/main.js
+++ example-app/src/main.js
@@ -1,3 +1,5 @@
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { Capacitor } from '@capacitor/core';
 import './style.css';
 import { CapgoVideoThumbnails } from '@capgo/capacitor-video-thumbnails';
 
@@ -206,3 +208,9 @@ runButton.addEventListener('click', async () => {
 });
 
 populateActions();
+
+if (Capacitor.isNativePlatform()) {
+  CapacitorUpdater.notifyAppReady().catch((error) => {
+    console.error('Capgo notifyAppReady failed', error);
+  });
+}
